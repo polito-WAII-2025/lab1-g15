f638896e420a3031e21afc5a7438de98d633a773
angelodusba <angelodusba@gmail.com>
2025-03-12 17:11:44 +0100

    chore: Waypoint data and service created

---
+++ RouteAnalyzer/src/main/kotlin/data/Waypoint.kt
@@ -0,0 +1,3 @@
+package org.routeanalyzer.data
+
+data class Waypoint(val timestamp: Long, val lat: Float, val long: Float)
---
+++ RouteAnalyzer/src/main/kotlin/service/WaypointService.kt
@@ -0,0 +1,7 @@
+package org.routeanalyzer.service
+
+import org.routeanalyzer.data.Waypoint
+
+class WaypointService(private val waypoints: List<Waypoint>) {
+
+}
